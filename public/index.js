289d78e327e120d4be6dc5ec20707c59ef348e13
Randy-Ryan <44271616+Randy-Ryan@users.noreply.github.com>
2021-05-28 12:44:44 -0400

    r

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
-window.onload = function() {
-    //set the loading screen for a few seconds - then route to home page
-    //TODO make index.html a loading screen
+// window.onload = function() {
+//     //set the loading screen for a few seconds - then route to home page
+//     //TODO make index.html a loading screen
     
-     setTimeout(function(){ 
-      window.location.href = "./AboutPage/about.html";
-       }, 2000);
+//      setTimeout(function(){ 
+//       window.location.href = "./AboutPage/about.html";
+//        }, 2000);
   
-  }
+//   }
