fc45af661150fefd4537ad3b28a8ae810a668704
Randy-Ryan <44271616+Randy-Ryan@users.noreply.github.com>
2021-06-16 00:08:49 -0400

    r

--- public/index.js
+++ public/index.js
@@ -32,6 +32,60 @@ window.onload = function () {
     document.getElementById("resumePage").style.display = "none";
     document.getElementById("laxPage").style.display = "none";
 
+    document.getElementById('message').addEventListener('keydown', function (e) {
+        if (e.key == 'Tab') {
+            // e.preventDefault();
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            // set textarea value to: text before caret + tab + text after caret
+            this.value = this.value.substring(0, start) +
+                "\t" + this.value.substring(end);
+
+            // put caret at right position again
+            this.selectionStart =
+                this.selectionEnd = start + 1;
+        }
+    });
+    var mainForm = document.getElementById("contact-form");
+    mainForm.onsubmit = function (e) {
+        //error2 var needed for when first submission is invalid
+        e.preventDefault();
+        //but second submission is correct
+        var error = false;
+        var error2 = false;
+        //initialize inputs variable from all inputs with class "required"
+        var requiredInputs = document.querySelectorAll(".required");
+        for (var i = 0; i < requiredInputs.length; i++) {
+            error = false;
+            console.log(requiredInputs[0].value + "   " + requiredInputs[1].value);
+            //if blank input, prevent form submission
+            //make input fields red and set error to true
+            if (isBlank(requiredInputs[i])) {
+                e.preventDefault();
+                makeRed(requiredInputs[i]);
+                error = true;
+                error2 = true;
+            }
+            //make input field normal if no error
+            //usage when first case is error, but second is not
+            if (error == false) {
+                makeClean(requiredInputs[i]);
+            }
+        }
+        if (error2 == false) {
+            //no input errors, should create a post/send me an email
+            // these IDs from the previous steps
+            emailjs.sendForm('contact_service', 'contact_form', this)
+                .then(function () {
+                    document.getElementById("contact-form").reset();
+                    alert('Email successfully sent!');
+                }, function (error) {
+                    alert('Failed to send email, please try again!');
+                    console.log('FAILED...', error);
+                });
+        }
+    }
     //style the feed
 
 }
@@ -107,4 +161,40 @@ function loadLaxPage() {
     document.getElementById("projectPage").style.display = "none";
     document.getElementById("resumePage").style.display = "none";
     document.getElementById("laxPage").style.display = "";
+}
+
+
+
+//function to check if input field is blank
+function isBlank(inputField) {
+    //checks for empty checkbox
+    if (inputField.type === "checkbox") {
+        if (inputField.checked) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    //checks for empty input fields
+    if (inputField.value === "") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+//function to make the input fields red
+function makeRed(inputDiv) {
+    //input field set to red
+    inputDiv.style.backgroundColor = "#c80815";
+}
+
+//function to reset the input fields to normal
+function makeClean(inputDiv) {
+    //text set to black
+    inputDiv.parentNode.style.color = "#000000";
+    //text field set to white	
+    inputDiv.style.backgroundColor = "#FFFFFF";
+
 }
