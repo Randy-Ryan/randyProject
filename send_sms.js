1b7e9a152786b6c1f02e64cfc93f63b8a1ac97bd
Randy-Ryan <44271616+Randy-Ryan@users.noreply.github.com>
2021-06-19 19:37:39 -0400

    r

--- send_sms.js
+++ send_sms.js
@@ -1,13 +1,13 @@
 
 
 
-    var twilio = require('twilio');
-    var client = new twilio('ACfe485fc20f11da0bc9dba9ac534de18b', '70177ea29ad3bce5927373cb9fae0c5c');
-    client.messages.create({ 
-      to: "YOUR_NUMBER",
-      from: "+19402414167",
-      body: "REMINDER_FORMAT",
-    }, function(err, message) {
-      console.log(message.sid);
-    }); 
+    // var twilio = require('twilio');
+    // var client = new twilio('', '');
+    // client.messages.create({ 
+    //   to: "YOUR_NUMBER",
+    //   from: "+19402414167",
+    //   body: "REMINDER_FORMAT",
+    // }, function(err, message) {
+    //   console.log(message.sid);
+    // }); 
     
